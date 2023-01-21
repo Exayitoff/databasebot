f4843a788ccb258fb733df07b610f3ac80e5c2fa
Exayitoff <104882895+Exayitoff@users.noreply.github.com>
2023-01-21 21:38:28 +0500

    Update main.py

--- main.py
+++ main.py
@@ -41,17 +41,20 @@ async def admin_panel(message : types.Message):
     else:
       await bot.send_message(chat_id=message.from_user.id, text="<i> Kechirasiz </i> <b>" + str(message.from_user.first_name) + "</b> <i> siz admin emassiz 😝 \n \n Bot admini: @MrXayitov </i>", parse_mode="HTML")
 
-@dp.callback_query_handler()
+@dp.callback_query_handler(text="btnRandom")
 async def randomize(calb: types.CallbackQuery):
-  try:
-    match calb.data:
-      case "btnRandom":
           link = 'https://dog.ceo/api/breeds/image/random'
           response = requests.get(link)
           itlar= response.json()
           xbr=itlar['message']
           await bot.send_photo(calb.from_user.id, xbr , caption="Kuchuklar ❤️", reply_markup=nav.mainMenu)
-      case "btnRandom2":
+
+
+
+
+
+@dp.callback_query_handler(text="btnRandom2")
+async def randomize(calb: types.CallbackQuery):  
           link = 'https://api.thecatapi.com/v1/images/search?limit=1'
           response = requests.get(link)
           itlar= response.json()
@@ -59,7 +62,10 @@ async def randomize(calb: types.CallbackQuery):
           for i in itlar :
             xbr+=i['url']
           await bot.send_photo(calb.from_user.id, xbr , caption="Mushuklar ❤️", reply_markup=nav.mainMenu)
-      case "btnRandom3":
+
+
+@dp.callback_query_handler(text="btnRandom3")
+async def randomize(calb: types.CallbackQuery):  
         taxmin = random.randint(1, 19)
         link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=snakes&image_type=photo&pretty='+str(taxmin)
         response = requests.get(link)
@@ -69,7 +75,8 @@ async def randomize(calb: types.CallbackQuery):
         for n in hts:
           lists.append(n["largeImageURL"])
         await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Ilonlar 🐍", reply_markup=nav.mainMenu)
-      case "btnRandom4":
+@dp.callback_query_handler(text="btnRandom4")
+async def randomize(calb: types.CallbackQuery):
         taxmin = random.randint(1, 20)
         link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=rabbit&image_type=photo&pretty='+str(taxmin)
         response = requests.get(link)
@@ -78,10 +85,11 @@ async def randomize(calb: types.CallbackQuery):
         lists = []
         for n in hts:
           lists.append(n["largeImageURL"])
-
         await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Quyonlar 🐇", reply_markup=nav.mainMenu)
 
-      case "btnRandom5":
+
+@dp.callback_query_handler(text="btnRandom5")
+async def randomize(calb: types.CallbackQuery):  
         taxmin = random.randint(1, 20)
         link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=animal+mouse&image_type=photo&pretty='+str(taxmin)
         response = requests.get(link)
@@ -92,7 +100,8 @@ async def randomize(calb: types.CallbackQuery):
           lists.append(n["largeImageURL"])
         await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Sichqonlar  🐁", reply_markup=nav.mainMenu)
 
-      case "btnRandom6":
+@dp.callback_query_handler(text="btnRandom6")
+async def randomize(calb: types.CallbackQuery): 
         taxmin = random.randint(1, 19)
         link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=birds&image_type=photo&pretty='+str(taxmin)
         response = requests.get(link)
@@ -102,9 +111,12 @@ async def randomize(calb: types.CallbackQuery):
         for n in hts:
           lists.append(n["largeImageURL"])
         await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Qushlar  🦚", reply_markup=nav.mainMenu)
-      case "boshqa":
+@dp.callback_query_handler(text="boshqa")
+async def randomize(calb: types.CallbackQuery): 
         await bot.send_photo(chat_id=calb.from_user.id, photo="https://i.pinimg.com/564x/ba/92/7f/ba927ff34cd961ce2c184d47e8ead9f6.jpg", caption=" <i>Assalomu alaykum </i> <b>"+calb.from_user.first_name + "</b> <i>hush kelibsiz bo'limlardan birini tanlang va sizga bot qiziqarli rasmlar jo\'natadi</i>",parse_mode="HTML", reply_markup=nav.bolm2)
-      case "otlar":
+
+@dp.callback_query_handler(text="otlar")
+async def randomize(calb: types.CallbackQuery):  
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=horses&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -114,7 +126,8 @@ async def randomize(calb: types.CallbackQuery):
           for n in hts:
             lists.append(n["largeImageURL"])
           await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Otlar 🐎", reply_markup=nav.bolm2)
-      case "pandalar":
+@dp.callback_query_handler(text="pandalar")
+async def randomize(calb: types.CallbackQuery): 
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=white+panda&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -124,7 +137,8 @@ async def randomize(calb: types.CallbackQuery):
           for n in hts:
             lists.append(n["largeImageURL"])
           await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Pandalar 🐼", reply_markup=nav.bolm2)
-      case "pinvgin":
+@dp.callback_query_handler(text="pinvgin")
+async def randomize(calb: types.CallbackQuery):  
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=pinguin&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -134,7 +148,8 @@ async def randomize(calb: types.CallbackQuery):
           for n in hts:
             lists.append(n["largeImageURL"])
           await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Pingvinlar 🐧", reply_markup=nav.bolm2)
-      case "sherlar":
+@dp.callback_query_handler(text="sherlar")
+async def randomize(calb: types.CallbackQuery):  
         taxmin = random.randint(1, 19)
         link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=lion&image_type=photo&pretty='+str(taxmin)
         response = requests.get(link)
@@ -144,7 +159,8 @@ async def randomize(calb: types.CallbackQuery):
         for n in hts:
           lists.append(n["largeImageURL"])
         await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Sherlar 🦁", reply_markup=nav.bolm2)
-      case "tovus":
+@dp.callback_query_handler(text="tovus")
+async def randomize(calb: types.CallbackQuery):  
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=peacock&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -154,7 +170,9 @@ async def randomize(calb: types.CallbackQuery):
           for n in hts:
             lists.append(n["largeImageURL"])
           await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Tovuslar 🦚", reply_markup=nav.bolm2)
-      case "tovuq":
+ 
+@dp.callback_query_handler(text="tovuq")
+async def randomize(calb: types.CallbackQuery):            
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=chicken&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -164,19 +182,27 @@ async def randomize(calb: types.CallbackQuery):
           for n in hts:
             lists.append(n["largeImageURL"])
           await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Kulguli Tovuqlar 🐓 ", reply_markup=nav.bolm2)
-      case "ortga":
+@dp.callback_query_handler(text="ortga")
+async def randomize(calb: types.CallbackQuery):        
         await bot.send_photo(chat_id=calb.from_user.id, photo="https://w0.peakpx.com/wallpaper/368/742/HD-wallpaper-cool-cat.jpg", caption="Siz orqaga qaytdingiz 😜", reply_markup=nav.mainMenu)
-      case "stats":
+
+@dp.callback_query_handler(text="stats")
+async def randomize(calb: types.CallbackQuery):  
         users = db.get_users()
         son=sum(1 for line in users)
-        await bot.send_message(chat_id=calb.message.chat.id, text="<i>👨🏻‍💻 Obunachilar soni - </i> <b>"+str(son)+" </b>  ta. \n \n📊@hayvonlar_rasmibot  <i> statistikasi  </i>",parse_mode="HTML")
-      case "userfayl":
+        await bot.send_message(chat_id=calb.message.chat.id, text="<i>👨🏻‍💻 Obunachilar soni - </i> <b>"+str(son)+" </b>  ta. \n \n📊@hayvonlar_rasmibot  <i> statistikasi  </i>",parse_mode="HTML")        
+@dp.callback_query_handler(text="userfayl")
+async def randomize(calb: types.CallbackQuery):
         users = db.get_users()
         od=[line for line in users]
         await bot.send_message(chat_id=admin , text=str(od))
-      case "boshqa2":
+        
+@dp.callback_query_handler(text="boshqa2")
+async def randomize(calb: types.CallbackQuery):  
         await bot.send_photo(chat_id=calb.from_user.id, photo="https://mobimg.b-cdn.net/v3/fetch/c7/c78069a2d7416ad906cc92b05a71c082.jpeg", caption="Ajoyib siz endi 3-bo'limdasiz \nHayvonlardan birini tanlang", reply_markup=nav.bolm3)
-      case "sigir":
+      
+@dp.callback_query_handler(text="sigir")
+async def randomize(calb: types.CallbackQuery):        
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=cow&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -186,7 +212,8 @@ async def randomize(calb: types.CallbackQuery):
           for n in hts:
             lists.append(n["largeImageURL"])
           await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Sigirlar 🐄 ", reply_markup=nav.bolm3)
-      case "bori":
+@dp.callback_query_handler(text="bori")
+async def randomize(calb: types.CallbackQuery):     
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=wolf&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -195,8 +222,9 @@ async def randomize(calb: types.CallbackQuery):
           lists = []
           for n in hts:
             lists.append(n["largeImageURL"])
-          await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Bo\'rilar 🐺", reply_markup=nav.bolm3)
-      case "gepard":
+          await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Bo\'rilar 🐺", reply_markup=nav.bolm3)   
+@dp.callback_query_handler(text="gepard")
+async def randomize(calb: types.CallbackQuery):        
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=leopard&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -206,7 +234,8 @@ async def randomize(calb: types.CallbackQuery):
           for n in hts:
             lists.append(n["largeImageURL"])
           await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Leopard 🐆", reply_markup=nav.bolm3)
-      case "fil":
+@dp.callback_query_handler(text="fil")
+async def randomize(calb: types.CallbackQuery):   
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=elephant&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -215,8 +244,9 @@ async def randomize(calb: types.CallbackQuery):
           lists = []
           for n in hts:
             lists.append(n["largeImageURL"])
-          await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Fillar 🐘", reply_markup=nav.bolm3)
-      case "kenguru":
+          await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Fillar 🐘", reply_markup=nav.bolm3)     
+@dp.callback_query_handler(text="kenguru")
+async def randomize(calb: types.CallbackQuery):  
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=kangaroo&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -225,8 +255,9 @@ async def randomize(calb: types.CallbackQuery):
           lists = []
           for n in hts:
             lists.append(n["largeImageURL"])
-          await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Kenguru 🦘", reply_markup=nav.bolm3)
-      case "kiyik":
+          await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Kenguru 🦘", reply_markup=nav.bolm3)      
+@dp.callback_query_handler(text="kiyik")
+async def randomize(calb: types.CallbackQuery): 
           taxmin = random.randint(1, 19)
           link = 'https://pixabay.com/api/?key=32452369-fb4ecba71876a7c19595878f8&q=deer&image_type=photo&pretty='+str(taxmin)
           response = requests.get(link)
@@ -235,13 +266,7 @@ async def randomize(calb: types.CallbackQuery):
           lists = []
           for n in hts:
             lists.append(n["largeImageURL"])
-          await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Kiyiklar 🦌", reply_markup=nav.bolm3)
-
-      case _:
-        await bot.send_message(calb.from_user.id, text="Boshqa rasmlarni ham sinab koring")
-
-  except Exception as e:
-    await bot.send_message(admin, text="Botda xatolik :"+str(e))
+          await bot.send_photo(calb.from_user.id, lists[taxmin] , caption="Kiyiklar 🦌", reply_markup=nav.bolm3) 
 
 
 
